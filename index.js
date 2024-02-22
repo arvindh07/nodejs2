6e4848f262c072e78a4da7ed97e395479a899d3b
arvindh07 <arvindhganesh11@gmail.com>
2024-02-22 10:26:22 +0530

    handled routing

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const server = http.createServer((req, res) => {
         const logText = date.toUTCString() + "\t" + req.method + "\t" + req.url + "\n";
         logger(logText);
     }
-    return res.end("<h1>hello from server!!</h1>");
+    if(req.url === "/"){
+        return res.end("<h1>Home page</h1>");
+    }else if(req.url === "/about"){
+        return res.end("<h1>About page</h1>");
+    } else{
+        return res.end("<h1>404 not found</h1>");
+    }
 })
 
 server.listen(PORT, () => {
