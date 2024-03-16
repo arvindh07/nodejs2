5926c6a708b2527a294b08c1818e0ef35301e37c
arvindh07 <arvindhganesh11@gmail.com>
2024-03-16 14:50:28 +0530

    server side socket added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 const path = require("path");
+const {Server} = require("socket.io");
+const io = new Server(server);
 
 // add public
 app.use(express.static(path.resolve("./public")));
@@ -10,6 +12,11 @@ app.use(express.static(path.resolve("./public")));
 // app for handling routes
 app.get("/", (req, res) => {
     return res.sendFile("/public/client.html", {root: __dirname});
+})
+
+// websocket
+io.on("connection", (socket) => {
+    console.log("server socket running ", socket);
 })
 
 // http server
