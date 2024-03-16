825cc0b249d72238f31d39b37b772abfedfdda17
arvindh07 <arvindhganesh11@gmail.com>
2024-03-16 12:26:03 +0530

    created express app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hlo from websockets");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.status(200).end("<h1>Hello from websocket app</h1>");
+})
+
+app.listen(8000, () => {
+    console.log("Server running on port 8000");
+});
